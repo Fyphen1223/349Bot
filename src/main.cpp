32f38d1ac5081822542dc4af817e0f865f4dd39a
Fyphen1223 <fyphensub@gmail.com>
2024-12-20 21:17:02 +0900

    .clang-formatの設定をLLVMスタイルに変更し、main.cppのコードスタイルを整形

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,33 @@
 #include <iostream>
 
 int print(std::string message) {
-    std::cout << message << std::endl;
-    return 0;
+	std::cout << message << std::endl;
+	return 0;
 }
 
 int main() {
-    /* Create the bot, but with our intents so we can use messages. */
-    dpp::cluster bot(
-	"MTI4MjUyMTY2NzQ1Njg2NDI5Ng.GC6b6V.jhNACAu1nZye5_qGxVieuC_"
-	"Rjqn7VtNE7bUhGs",
-	dpp::i_default_intents | dpp::i_message_content);
+	/* Create the bot, but with our intents so we can use messages. */
+	dpp::cluster bot(
+		"MTI4MjUyMTY2NzQ1Njg2NDI5Ng.GC6b6V.jhNACAu1nZye5_qGxVieuC_"
+		"Rjqn7VtNE7bUhGs",
+		dpp::i_default_intents | dpp::i_message_content);
 
-    bot.on_log(dpp::utility::cout_logger());
+	bot.on_log(dpp::utility::cout_logger());
 
-    /* The event is fired when the bot detects a message in any server and any
+	/* The event is fired when the bot detects a message in any server and any
      * channel it has access to. */
-    bot.on_message_create([&bot](const dpp::message_create_t& event) {
-	/* See if the message contains the phrase we want to check for.
+	bot.on_message_create([&bot](const dpp::message_create_t &event) {
+		/* See if the message contains the phrase we want to check for.
 	 * If there's at least a single match, we reply and say it's not
 	 * allowed.
 	 */
-	if (event.msg.content.find("bad word") != std::string::npos) {
-	    event.reply("That is not allowed here. Please, mind your language!",
-			true);
-	}
-    });
+		if (event.msg.content.find("bad word") != std::string::npos) {
+			event.reply("That is not allowed here. Please, mind your language!",
+						true);
+		}
+	});
 
-    bot.start(dpp::st_wait);
+	bot.start(dpp::st_wait);
 
-    print("Bot started!");
-
-    return 0;
+	return 0;
 }
