bb2e8b236daaf029142ad41ba438037c0b3fcdb1
Fyphen1223 <fyphensub@gmail.com>
2025-01-01 20:59:39 +0900

    音楽再生コマンドのスラッシュコマンド処理を追加（play, pause, resume, search, stop, skip, back, volume, queue, now playing, loop, shuffle, lyrics, seek）。

--- src/events/slashcommandsCreate.cpp
+++ src/events/slashcommandsCreate.cpp
@@ -10,6 +10,49 @@ void executePing(dpp::cluster &bot, const dpp::slashcommand_t &event) {
 	event.reply("Pong! REST Ping: " + getRestPing(bot));
 }
 
+void executePlay(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executePause(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeResume(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeSearch(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeStop(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeSkip(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeBack(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeVolume(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeQueue(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeNowPlaying(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeLoop(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeShuffle(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeLyrics(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+void executeSeek(dpp::cluster &bot, const dpp::slashcommand_t &event) {
+}
+
+
 void onSlashCommands(dpp::cluster &bot, const dpp::slashcommand_t &event) {
 	const std::string commandName = event.command.get_command_name();
 
