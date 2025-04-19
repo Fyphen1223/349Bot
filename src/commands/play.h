33e29b992595a878d5d3b9a46e90a1022c964bbb
Fyphen1223 <fyphensub@gmail.com>
2025-04-19 20:01:30 +0900

    プレイコマンドを追加し、コマンド初期化に登録。関連ヘッダーファイルをインクルード。

--- src/commands/play.h
+++ src/commands/play.h
@@ -1,14 +1,13 @@
 #ifndef PLAY_H
 #define PLAY_H
 
+#include "../global.h"
+#include <dpp/dpp.h>
 #include <iostream>
 #include <string>
 
-class PlayCommand {
-  public:
-	void ping() {
-		std::cout << "Pong!" << std::endl;
-	}
-};
+#include <dpp/dpp.h>
+
+void Play(dpp::cluster &bot, const dpp::slashcommand_t &event);
 
 #endif// PLAY_H
--- src/events/commands.cpp
+++ src/events/commands.cpp
@@ -1,8 +1,10 @@
 #include "commands.h"
 #include "../commands/ping.h"
+#include "../commands/play.h"
 
 std::map<std::string, std::function<void(dpp::cluster &bot, const dpp::slashcommand_t &event)>> Commands;
 
 void initializeCommands() {
 	Commands["ping"] = Ping;
+	Commands["play"] = Play;
 }
